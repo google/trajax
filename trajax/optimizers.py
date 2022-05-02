a14d248294e9c8f74f10a10e787af24ac6be1ad7
trajax authors <no-reply@google.com>
2022-05-02 10:43:58 -0400

    Remove unused comments related to Python 2 compatibility.
    PiperOrigin-RevId: 445930330

--- trajax/optimizers.py
+++ trajax/optimizers.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Lint as: python3
 # pylint: disable=invalid-name
 """Building Blocks for Gradient-based Trajectory Optimizers.
 
